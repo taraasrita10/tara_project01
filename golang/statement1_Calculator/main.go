c327261ef6ecbb4060525f7a049aef02ca8ba220
Prabhala Tara Aasrita <prabhalatara@Prabhalas-MacBook-Air.local>
2026-02-09 12:55:00 +0530

    Updated Calculator with corrections

--- golang/statement1_Calculator/main.go
+++ golang/statement1_Calculator/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+//Calculator function :
+// Takes two operators and an operand as input, performs
+// a mathematical function and provides the output.
+// Involves addition, subtraction, multiplication and division of float64 numbers.
+
 func calculator(op1, op2 float64, op string) float64 {
 	switch op {
 	case "+":
@@ -28,25 +33,27 @@ func main() {
 	var use string
 	fmt.Println("Would you like to use the calculator? (y/n)")
 	fmt.Scan(&use)
-	for use != "n" {
+	for use == "y" {
 		fmt.Println("Enter the first operand : ")
-		fmt.Scan(&op1)
+		_, err1 := fmt.Scan(&op1)
 		fmt.Println("Enter the second operand : ")
-		fmt.Scan(&op2)
+		_, err2 := fmt.Scan(&op2)
+		if err1 != nil || err2 != nil {
+			fmt.Println("Invalid Operand!")
+			continue
+		}
 		fmt.Println("Enter the operator : ")
 		fmt.Scan(&op)
-		result := calculator(op1, op2, op)
-		if result == 0 {
-			if op2 == 0 && op == "/" {
-				fmt.Println("Division with 0 is not possible.")
-			} else if op != "+" && op != "-" && op != "*" && op != "/" {
-				fmt.Println("Operator not valid")
-			} else {
-				fmt.Println(result)
-			}
-		} else {
-			fmt.Println(result)
+		if op != "+" && op != "-" && op != "*" && op != "/" {
+			fmt.Println(("Invalid Operator!"))
+			continue
 		}
+		if op2 == 0 && op == "/" {
+			fmt.Println("Division by 0 is not possible")
+			continue
+		}
+		result := calculator(op1, op2, op)
+		fmt.Printf("%g %s %g = %g\n", op1, op, op2, result)
 		fmt.Println("Would you like to continue using the calculator? (y/n)")
 		fmt.Scan(&use)
 		if use != "y" && use != "n" {
